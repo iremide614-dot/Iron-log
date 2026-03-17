6b63405de0928906338404c3ae98323a5cb9c18c
iremide614-dot <iremide614@gmail.com>
2026-03-16 19:08:46 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const V='recomp-v1';
-const FILES=['/','/index.html','/app.js','/manifest.json','/icon-192.png','/icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(V).then(c=>c.addAll(FILES)));self.skipWaiting()});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==V).map(k=>caches.delete(k)))));self.clients.claim()});
+const V='recomp-v2';
+const FILES=['./','/index.html','./app.js','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
